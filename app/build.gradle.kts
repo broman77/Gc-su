004f1e9c1c465afa95c3bcfa6ef25e2b0fbc5dcf
broman77 <71395385+broman77@users.noreply.github.com>
2026-09-18 17:01:21 +0300

    Bump app to beta 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.su555.defects"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.5.0"
     }
 
     buildTypes {
