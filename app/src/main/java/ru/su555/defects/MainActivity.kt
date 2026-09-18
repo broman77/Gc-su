b7ae84b82477d3e07688ec953a742052ae739f42
broman77 <71395385+broman77@users.noreply.github.com>
2026-09-18 09:09:05 +0300

    Add MainActivity.kt

---
+++ app/src/main/java/ru/su555/defects/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.su555.defects
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Su555Theme {
+                DefectsApp()
+            }
+        }
+    }
+}
